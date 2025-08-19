4fd3972028604f4683120c8cca3855b835766d64
동훈 강 <rkdehdgns512@gmail.com>
2025-08-19 14:41:48 +0900

    many camera capture

--- plump.py
+++ plump.py
@@ -102,20 +102,34 @@ scene.rigidbody_world.effector_weights.gravity = 1.0
 
 
 # 카메라 추가
-bpy.ops.object.camera_add(location=(15, -15, 10))
-camera = bpy.context.active_object
-camera.rotation_euler = (1.1, 0, 0.785)
+cameras = []
+target  = mathutils.Vector((0, 0, 0))
+x_range = (10, 15)
+y_range = (10, 15)
+z_range = 10
+
+def random_value(min_val, max_val):
+    if np.random.rand() < 0.5:
+        return random.uniform(min_val, max_val)
+    else:
+        return random.uniform(-max_val, -min_val)
+
+for i in range(14):
+    x_value, y_value, z_value = random_value(*x_range), random_value(*y_range), z_range
+    bpy.ops.object.camera_add(location=(x_value, y_value, z_value))
+    camera = bpy.context.active_object
+    direction = target - camera.location
+    rot_quat = direction.to_track_quat('-Z', 'Y')
+    camera.rotation_euler = rot_quat.to_euler()
+    camera.name = f"Camera_{i+1}"
+    cameras.append(camera)
+
 bpy.context.scene.camera = camera
 
 # 조명 추가
-bpy.ops.object.light_add(type='SUN', location=(5, 5, 10))
+bpy.ops.object.light_add(type='SUN', location=(np.random.uniform(-20,20), np.random.uniform(-20,20), 20))
 sun_light = bpy.context.active_object
-sun_light.data.energy = 3
-
-for obj in bpy.context.scene.objects:
-    for key in obj.keys():
-        if key != "_RNA_UI":
-            print(f"{key}: {obj[key]}")
+sun_light.data.energy = np.random.uniform(1, 7)
 
 
 # BlenderProc 렌더링 설정
@@ -132,27 +146,37 @@ scene.frame_set(frame_start)
 # scene.rigidbody_world.point_cache.frame_end = 250
 bpy.ops.ptcache.bake_all(bake=True)
 
+# BlenderProc 카메라 포즈 초기화 (기존 포즈 제거)
+bproc.camera.set_resolution(512, 512)
+
 # 시뮬레이션 완료 후 특정 프레임만 렌더링
 for frame in render_frames:
-    bpy.context.scene.frame_set(frame)
-    bpy.context.view_layer.update()
-    
-    # 한 번에 모든 데이터 렌더링
-    bpy.context.scene.frame_start = frame
-    bpy.context.scene.frame_end = frame + 1
-    data = bproc.renderer.render()
-    print(data.keys())
-    print(data)
-    frame_output_dir = os.path.join(output_dir, f"frame_{frame:04d}")
-    os.makedirs(frame_output_dir, exist_ok=True)
-    
-    bproc.writer.write_hdf5(f'./output/hdf5', data)
-    
-    # # COCO 형식의 annotation JSON 저장
-    bproc.writer.write_coco_annotations(
-        os.path.join(frame_output_dir, "coco_annotations.json"),
-        instance_segmaps=data.get("category_id_segmaps", []),
-        instance_attribute_maps=data.get("instance_attribute_maps", []),
-        colors=data["colors"],
-        color_file_format="JPEG"
-    )
+    for i, cam in enumerate(cameras):
+        # BlenderProc 카메라 포즈 설정 (각 카메라마다)
+        cam_matrix = cam.matrix_world
+        bproc.camera.add_camera_pose(cam_matrix)
+        
+        bpy.context.scene.frame_set(frame)
+        bpy.context.view_layer.update()
+        
+        # 한 번에 모든 데이터 렌더링
+        bpy.context.scene.frame_start = frame
+        bpy.context.scene.frame_end = frame + 1
+        data = bproc.renderer.render()
+        
+        frame_output_dir = os.path.join(output_dir, f"hdf5")
+        os.makedirs(frame_output_dir, exist_ok=True)
+        
+        bproc.writer.write_hdf5(output_dir + f'/hdf5/{frame:04d}_{cam.name}.hdf5', data)
+        
+        # # COCO 형식의 annotation JSON 저장
+        bproc.writer.write_coco_annotations(
+            os.path.join(frame_output_dir, "coco_annotations.json"),
+            instance_segmaps=data.get("category_id_segmaps", []),
+            instance_attribute_maps=data.get("instance_attribute_maps", []),
+            colors=data["colors"],
+            color_file_format="JPEG"
+        )
+        
+        # 다음 카메라를 위해 현재 포즈 제거
+        bproc.camera.set_resolution(512, 512)  # 포즈 리셋
